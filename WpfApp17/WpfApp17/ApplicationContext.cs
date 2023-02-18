7b9f637b1789059491c44f4e68063d7f03a45ff3
ShevchenkoDmytriy <dimasevcenko506@gmail.com>
2023-02-18 21:38:51 +0200

    c#

---
+++ WpfApp17/WpfApp17/ApplicationContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<User> Users { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Data Source = HOME-PC;Initial Catalog = adonetdb;Trusted_Connection=True;TrustServerCertificate= True");
+    }
+}
