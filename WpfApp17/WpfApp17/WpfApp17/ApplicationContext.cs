e048ab6da85dcf5bcefbfee66390893a54cd1049
ShevchenkoDmytriy <dimasevcenko506@gmail.com>
2023-02-21 20:58:59 +0200

    c#

---
+++ WpfApp17/WpfApp17/WpfApp17/ApplicationContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<Book> Books { get; set; } = null!;
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Data Source = HOME-PC; Initial Catalog = Shop; Trusted_Connection=True; TrustServerCertificate=True");
+    }
+}
---
+++ WpfApp17/WpfApp17/WpfApp17/User.cs
@@ -0,0 +1,9 @@
+﻿public class Book
+{
+    public int Id { get; set; } 
+    public string? Name { get; set; }
+    public int Price { get; set; }
+    public string? About { get; set; }
+    public int Num { get; set; }
+    public string? Coment { get; set; }
+}
